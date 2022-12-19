8538ca5f94d3f29389595b0e7167f20f1f9f121f
sethreidnz <contact@sethreid.co.nz>
2022-12-20 11:09:16 +1300

    minal repo

--- src/useRepositoryQuery.test.tsx
+++ src/useRepositoryQuery.test.tsx
@@ -1,11 +1,8 @@
 import { QueryStatus, useRepositoryQuery } from "./useRepositoryQuery";
 import { renderHook } from "@testing-library/react-hooks";
+import { render, screen } from "@testing-library/react";
 import { observer } from "mobx-react";
 import { ReactNode } from "react";
-
-const Wrapper = observer(({ children }: { children: ReactNode }) => {
-  return <>{children}</>;
-});
 
 describe("useMobxExampleHook()", () => {
   test("given successful call to fetchFn should return success", async () => {
@@ -15,6 +12,9 @@ describe("useMobxExampleHook()", () => {
     const fetchFn = vi.fn().mockImplementation(() => fetchPromise);
     const queryKey = ["query-key-1"];
     const options = { getFn, fetchFn, queryKey };
+    const Wrapper = observer(({ children }: { children: ReactNode }) => {
+      return <>{children}</>;
+    });
 
     // act
     const hook = renderHook(() => useRepositoryQuery(options), {
@@ -24,5 +24,25 @@ describe("useMobxExampleHook()", () => {
 
     // assert
     expect(hook.result.current.status).toBe(QueryStatus.Success);
+  });
+
+  test("given successful call to fetchFn should return success", async () => {
+    // arrange
+    const data = { data: "data" };
+    const getFn = vi.fn().mockReturnValue(undefined);
+    const fetchFn = vi.fn().mockResolvedValue(data);
+    const queryKey = ["query-key-1"];
+    const options = { getFn, fetchFn, queryKey };
+    const Wrapper = observer(() => {
+      const query = useRepositoryQuery(options);
+      if (query.isLoading) return null;
+      return <div>Success</div>;
+    });
+
+    // act
+    const hook = render(<Wrapper />);
+
+    // assert
+    await screen.findByText("Success");
   });
 });
